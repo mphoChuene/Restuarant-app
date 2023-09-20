17d3dac580677a6fc540d6e15f49e8c274b068e9
mphoChuene <mphochuene42@gmail.com>
2023-09-20 20:36:23 +0200

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+// import HomeScreen from "./Screens/Restaurant/HomeScreen";
+import RecipeDetailScreen from "./Screens/Restaurant/RecipeDetailScreen";
+import DATA from "./config/Restaurant/DATA";
+// import WelcomeScreen from "./screens/Restaurant/WelcomeScreen";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () => {
+  return <RecipeDetailScreen recipe={DATA[0].recipes[2]} />;
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
